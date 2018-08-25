cbb8f371cbddf5f727440bf2502e3b22f65a280f
DESKTOP-S4B16IQ\Zsolti <d.zsolti@yahoo.com>
2018-08-25 18:24:22 +0300

    confirm button update

--- ConfirmButton/src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { IonicModule } from 'ionic-angular';
-import { NgModule } from '@angular/core';
-import { ConfirmButtonComponent } from './confirm-button/confirm-button';
-@NgModule({
-	declarations: [ConfirmButtonComponent],
-	imports: [IonicModule],
-	exports: [ConfirmButtonComponent]
-})
-export class ComponentsModule {}
